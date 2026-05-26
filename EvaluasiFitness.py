5daef751ab6098ddf4da358f60ddc703e1c50b31
adjeng.dewi@mhs.unsoed.ac.id <adjeng.dewi@mhs.unsoed.ac.id>
2026-05-26 22:25:51 +0700

    program evaluasi fitness

--- EvaluasiFitness.py
+++ EvaluasiFitness.py
@@ -1,13 +1,14 @@
+# Fungsi untuk menghitung nilai fitness
 def hitung_fitness(kromosom, barang, kapasitas_gudang):
     total_keuntungan = 0
     total_ukuran = 0
     
     for i in range(len(kromosom)):
         if kromosom[i] == 1:
-            total_keuntungan += barang[i][1]
-            total_ukuran += barang[i][2]
+            total_keuntungan += barang[i][1]  # keuntungan
+            total_ukuran += barang[i][2]      # ukuran
     
     if total_ukuran > kapasitas_gudang:
-        return 0
+        return 0  # Penalti jika melebihi kapasitas
     else:
         return total_keuntungan
